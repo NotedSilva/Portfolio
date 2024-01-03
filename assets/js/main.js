039889c7f4ebf3b13d3a6063d88b6da1ad68c92a
Gabriel S. Jesus <gabriel_live12@outlook.com>
2024-01-03 20:55:30 -0300

    Criação de animações e ajustes de bugs visuais

--- assets/js/main.js
+++ assets/js/main.js
@@ -35,13 +35,63 @@ const shadowHeader = () =>{
 window.addEventListener('scroll', shadowHeader)
 
 /*=============== Configuração do email ===============*/
+const contactForm = document.getElementById('contact-form'),
+        contactMessage = document.getElementById('contact-message')
 
+const sendEmail = (e) =>{
+    e.preventDefault()
 
-/*=============== SHOW SCROLL UP ===============*/ 
+    // serviceID - templateID - #form - publicKey
 
+    emailjs.sendForm('service_l7vw5xj','template_p8bdukx','#contact-form','eD8NZyXKotJdvKhOn')
+    .then(() =>{
+        // Mostrar mensagem enviada
+        contactMessage.textContent = 'Mensagem enviada com sucesso ✅'
 
-/*=============== SCROLL SECTIONS ACTIVE LINK ===============*/
+        // Remover mensagem após cinco segundos
+        setTimeout(() =>{
+            contactMessage.textContent = ''
+        }, 5000)
 
+        // Limpar os campos
+        contactForm.reset()
+
+    }, () =>{
+        // Mostrar mensagem de erro
+        contactMessage.textContent = 'Mensagem não enviada (erro de serviço) ❌'
+    })
+}
+
+contactForm.addEventListener('submit', sendEmail)
+
+/*=============== Mostrar rolagem para cima ===============*/ 
+const scrollUp = () =>{
+    const scrollUp = document.getElementById('scroll-up')
+    // quando a rolagem for maior que 350 de altura na página, adicione essa condição
+    this.scrollY >= 350 ? scrollUp.classList.add('show-scroll')
+                        : scrollUp.classList.remove('show-scroll')
+}
+window.addEventListener('scroll', scrollUp)
+
+/*=============== Link das Seções de Rolagem ===============*/
+const sections = document.querySelectorAll('section[id]')
+
+const scrollActive = () =>{
+    const scrollDown = window.scrollY
+
+    sections.forEach(current =>{
+        const sectionHeight = current.offsetHeight,
+              sectionTop = current.offsetTop - 58,
+              sectionId = current.getAttribute('id'),
+              sectionClass = document.querySelector('.nav__menu a[href*=' + sectionId + ']')
+              
+        if(scrollDown > sectionTop && scrollDown <= sectionTop + sectionHeight){
+            sectionClass.classList.add('active-link')
+        }else{
+            sectionClass.classList.remove('active-link')
+        }
+    })
+}
 
 /*=============== DARK LIGHT THEME ===============*/ 
 
