53201b039426599cf0a240349541bd0b70d1a018
Gabriel S. Jesus <gabriel_live12@outlook.com>
2024-01-04 21:01:29 -0300

    Criação de Breakpoints

--- assets/js/main.js
+++ assets/js/main.js
@@ -92,8 +92,36 @@ const scrollActive = () =>{
         }
     })
 }
+window.addEventListener('scroll', scrollActive)
 
-/*=============== DARK LIGHT THEME ===============*/ 
+/*=============== Tema escuro ===============*/ 
+const themeButton = document.getElementById('theme-button')
+const darkTheme = 'dark-theme'
+const iconTheme = 'ri-sun-line'
 
+// Tópico selecionado anteriormente (se o usuário tiver selecionado)
+const selectedTheme = localStorage.getItem('selected-theme')
+const selectedIcon = localStorage.getItem('selected-icon')
+
+// Obtemos o tema atual que a interface possui validando a classe dark-theme
+const getCurrentTheme = () => document.body.classList.contains(darkTheme) ? 'dark' : 'light'
+const getCurrentIcon = () => themeButton.classList.contains(iconTheme) ? 'ri-moon-line' : 'ri-sun-line'
+
+// Validamos se o usuário escolheu um tema anteriormente
+if (selectedTheme) {
+    // Se a validação for cumprida, perguntamos qual foi o problema para saber se ativamos ou desativamos o dark
+    document.body.classList[selectedTheme === 'dark' ? 'add' : 'remove'](darkTheme)
+    themeButton.classList[selectedIcon === 'bx bx-moon' ? 'add' : 'remove'](iconTheme)
+}
+
+//Ativa/desativa o tema manualmente com o botão
+themeButton.addEventListener('click', () =>{
+    //Adiciona ou remove o tema escuro/ícone
+    document.body.classList.toggle(darkTheme)
+    themeButton.classList.toggle(iconTheme)
+    // Salvamos o tema e o ícone atual que o usuário escolheu
+    localStorage.setItem('selected-theme', getCurrentTheme())
+    localStorage.setItem('selected-icon', getCurrentIcon())
+})
 
 /*=============== SCROLL REVEAL ANIMATION ===============*/
