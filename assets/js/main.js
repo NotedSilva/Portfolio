35dcb980cec426074810d9d172ef604392abbfcc
Gabriel S. Jesus <gabriel_live12@outlook.com>
2024-01-05 20:14:23 -0300

    Pequenas alterações e melhorias nas animações

--- assets/js/main.js
+++ assets/js/main.js
@@ -124,4 +124,30 @@ themeButton.addEventListener('click', () =>{
     localStorage.setItem('selected-icon', getCurrentIcon())
 })
 
-/*=============== SCROLL REVEAL ANIMATION ===============*/
+/*=============== Animação do SCROLL REVEAL ===============*/
+const sr = ScrollReveal({
+    origin: 'top',
+    distance: '60px',
+    duration: 2500,
+    delay: 400,
+   // reset: true // repetições da animação
+})
+
+sr.reveal(`.home__perfil, .about__image, .contact__mail`, {origin: 'right'})
+sr.reveal(`.home__name, .home__info,
+           .about__container .section__title-1, .about__info,
+           .contact__social, .contact__data`, {origin: 'left'})
+sr.reveal(`.services__card, .projects__card`, {interval: 100})
+
+/*=============== DOWLOAD PDF ===============*/
+function confirmDownload() {
+    var choice = confirm("Deseja visualizar o currículo?");
+    
+    if (choice) {
+        // Se o usuário escolher "OK", permita que o link siga o comportamento padrão e abra o PDF no navegador.
+        return true;
+    } else {
+        // Se o usuário escolher "Cancelar", evite que o link siga o comportamento padrão (não faça nada).
+        return false;
+    }
+}
